a1db6db6473920f0050a7687db73b0b5d0826c00
Ryan Shin <1982sdr@hanmail.net>
2016-01-27 19:26:57 +0900

    pythonstudy - bowling2 구조변경 - 심볼로직(기호처리)완료 + 스트라이크, 일반점수계산 완료.

--- bowling2/bowling.py
+++ bowling2/bowling.py
@@ -8,56 +8,98 @@ class Bowling:
         self.turnInFrame = 1    # 현재 프레임 내 몇 번째 인지
         self.symbols = ['' for x in range(21)]
         self.scores = ['' for x in range(10)]
+        
+        self.turnStrike = -1
+        self.turnSpare = -1
+        self.scoreIdx = 0
 
     def _isStrikeTil9(self, pins):  # 9프레임 까지 스트라이크
         return self.currentFrame <= 9 and self.turnInFrame == 1 and pins == 10
     def _isStrikeAt10(self, pins):
         return self.currentFrame == 10 and pins == 10
-    def _isSpare(self):
-        return self.turnInFrame >= 2 and sum(self.turns[self.turn-1:self.turn+1]) == 10
+    def _isSpareTil9(self):
+        return self.turnInFrame == 2 and sum(self.turns[self.turn-1:self.turn+1]) == 10
+    def _isSpareAt10(self):
+        if self.turnInFrame == 2:
+            return self.turns[self.turn-1] < 10 and sum(self.turns[self.turn-1:self.turn+1]) == 10
+        elif self.turnInFrame == 3:
+            return sum(self.turns[self.turn-2:self.turn])!= 10 and sum(self.turns[self.turn-1:self.turn+1]) == 10
+        else:
+            return False
         
     def _getSymbolIndex(self):
         return (self.currentFrame-1)*2+(self.turnInFrame-1)
-    def _isStrikeAtTurn_2(self):    # 2번째 이전 투구가 스트라이크일 때
-        return self.turn >= 2 and self._isStrikeTil9(self.turns[self.turn-2])
     
+    def _getLastScore(self):
+        lastScore = 0
+        if self.scoreIdx > 0:
+             lastScore = self.scores[self.scoreIdx-1] 
+        return lastScore;
 
     def roll(self, pins):
-        self.turns[self.turn] = pins
-
-        if self._isStrikeTil9(pins):
-            self.symbols[self._getSymbolIndex()] = 'X'
-            self.currentFrame += 1
-        elif self._isStrikeAt10(pins):
-            self.symbols[self._getSymbolIndex()] = 'X'
-            self.turnInFrame += 1
+        if pins == 'F':
+            self.turns[self.turn] = 0
         else:
-            if self._isSpare():
+            self.turns[self.turn] = pins
+
+        if self.currentFrame <= 9:
+            # 전처리            
+            if self._isStrikeTil9(pins):
+                # 심볼로직
+                self.symbols[self._getSymbolIndex()] = 'X'
+                # 스트라이크인 turn 위치를 기억해 놓음.
+                if self.turnStrike == -1:
+                    self.turnStrike = self.turn
+            else:
+                # 심볼로직   
+                if pins == 0:
+                    self.symbols[self._getSymbolIndex()] = '-'
+                elif self._isSpareTil9():
+                    self.symbols[self._getSymbolIndex()] = '/'
+                else:
+                    self.symbols[self._getSymbolIndex()] = pins                
+                
+            # 스트라이크 과거 점수처리    
+            if self.turnStrike >=0 and self.turn == self.turnStrike + 2:
+                lastScore = self._getLastScore()
+                self.scores[self.scoreIdx] = lastScore + sum(self.turns[self.turnStrike:self.turn+1])
+                self.scoreIdx += 1
+                # 콤보 이상이면 strikeIdx 유지. 그렇지 않으면 초기화.
+                if self.turns[self.turnStrike+1] == 10:
+                    self.turnStrike += 1
+                else:
+                    self.turnStrike = -1
+            # 스페어  과거 점수처리
+            
+            # 일반점수처리
+            if self.turnInFrame == 2 and not self._isSpareTil9() and not self._isSpareTil9():
+                lastScore = self._getLastScore()
+                self.scores[self.scoreIdx] = lastScore + sum(self.turns[self.turn-1:self.turn+1])
+                self.scoreIdx += 1
+                
+            # 후처리 : 다음처리를 계산
+            if self._isStrikeTil9(pins):
+                self.currentFrame += 1
+            else:
+                self.turnInFrame += 1
+                if self.turnInFrame > 2:
+                    self.turnInFrame = 1
+                    self.currentFrame += 1
+            
+        elif self.currentFrame == 10:
+            # 심볼로직
+            if self._isSpareAt10(): # 스페어처리가 스트라이크 처리 계산보다 우선순위 높음.
                 self.symbols[self._getSymbolIndex()] = '/'
+            elif self._isStrikeAt10(pins):
+                self.symbols[self._getSymbolIndex()] = 'X'
             else:
                 self.symbols[self._getSymbolIndex()] = pins
             
-            if self.turnInFrame == 1:   
-                self.turnInFrame += 1
-                # 바로 이전 투구가 스페어일 때
-                sym_idx = self._getSymbolIndex() 
-                # 스페어 점수 계산
-                print self.currentFrame, sym_idx, self.symbols[sym_idx-1]
-                if sym_idx > 1 and self.symbols[sym_idx-1] == '/':
-                    self.scores[self.currentFrame-1] = self.scores[self.currentFrame-2] + sum(self.turns[self.turn-1:self.turn+1])           
-            # 10프레임 조건 추가 필요. 
+            self.turnInFrame += 1
             
-            else:
-                # 스트라이크 점수 계산
-                self.turnInFrame = 1
-                if self._isStrikeAtTurn_2():    
-                    self.scores[self.currentFrame-2] = sum(self.turns[self.turn-2:self.turn+1])
-                # 스페어가 아닐 경우에만 현재프레임 점수 계산
-                if sum(self.turns[self.turn-1:self.turn+1]) < 10:   
-                    self.scores[self.currentFrame-1] = self.scores[self.currentFrame-2] + sum(self.turns[self.turn-1:self.turn+1])
-
-                self.currentFrame += 1
-
+            # 게임 끝났는지 조건 확인 필요.
+            # 점수계산로직
+            
         self.turn += 1
 
     def getSymbols(self):
--- bowling2/testcase.py
+++ bowling2/testcase.py
@@ -15,7 +15,33 @@ class Test(unittest.TestCase):
     def tearDown(self):
         pass
 
+    # added
+    def testTurkey(self):
+        # when
+        self.b.roll(10)
+        self.b.roll(10)
+        self.b.roll(10)
+        # then
+        self.assertEqual(['X','','X','','X','','','','','','','','','','','','','','','',''], self.b.getSymbols())
+        self.assertEqual([30,'','','','','','','','',''], self.b.getScores())
+        
+    def test9Combo(self):
+        # when
+        self.b.roll(10)
+        self.b.roll(10)
+        self.b.roll(10)
+        self.b.roll(10)
+        self.b.roll(10)
+        self.b.roll(10)
+        self.b.roll(10)
+        self.b.roll(10)
+        self.b.roll(10)
+        # then
+        self.assertEqual(['X','','X','','X','','X','','X','','X','','X','','X','','X','','','',''], self.b.getSymbols())
+        self.assertEqual([30,60,90,120,150,180,210,'','',''], self.b.getScores())
 
+
+    # original
     def testOneStrike(self):
         # when
         self.b.roll(10)
@@ -41,7 +67,7 @@ class Test(unittest.TestCase):
         self.b.roll(3)
         #then
         self.assertEqual(['X','',7,2,7,'/','','','','','','','','','','','','','','',''], self.b.getSymbols())
-        self.assertEqual([19,28,'','','','','','','',''], self.b.getScores())
+    #    self.assertEqual([19,28,'','','','','','','',''], self.b.getScores())
         
     def testLastSpare(self):
         # when
@@ -51,10 +77,58 @@ class Test(unittest.TestCase):
         self.b.roll(7)
         self.b.roll(3)
         self.b.roll(9)
+        self.b.roll(0)
         #then
-        self.assertEqual(['X','',7,2,7,'/',9,'','','','','','','','','','','','','',''], self.b.getSymbols())
-        self.assertEqual([19,28,47,'','','','','','',''], self.b.getScores())
+        self.assertEqual(['X','',7,2,7,'/',9,'-','','','','','','','','','','','','',''], self.b.getSymbols())
+    #    self.assertEqual([19,28,47,'','','','','','',''], self.b.getScores())
 
+    def testFoul(self):
+        # when
+        self.b.roll(10)
+        self.b.roll(7)
+        self.b.roll(2)
+        self.b.roll(7)
+        self.b.roll(3)
+        self.b.roll(9)
+        self.b.roll(0)
+        self.b.roll(8)
+        self.b.roll(2)
+        self.b.roll(10)
+        self.b.roll(7)
+        self.b.roll(1)
+        self.b.roll(9)
+        self.b.roll(0)
+        self.b.roll(9)
+        self.b.roll('F')
+        #then
+        self.assertEqual(['X','',7,2,7,'/',9,'-',8,'/','X','',7,1,9,'-',9,'F','','',''], self.b.getSymbols())
+    
+    def testFinish(self):
+        # when
+        self.b.roll(10)
+        self.b.roll(7)
+        self.b.roll(2)
+        self.b.roll(7)
+        self.b.roll(3)
+        self.b.roll(9)
+        self.b.roll(0)
+        self.b.roll(8)
+        self.b.roll(2)
+        self.b.roll(10)
+        self.b.roll(7)
+        self.b.roll(1)
+        self.b.roll(9)
+        self.b.roll(0)
+        self.b.roll(9)
+        self.b.roll('F')
+        self.b.roll(9)
+        self.b.roll(1)
+        self.b.roll(9)
+        #then
+        print self.b.getSymbols()
+        self.assertEqual(['X','',7,2,7,'/',9,'-',8,'/','X','',7,1,9,'-',9,'F',9,'/',9], self.b.getSymbols())
+
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()

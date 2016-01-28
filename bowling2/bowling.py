7c547fbf456a287d2c75937ea77f5089830326e1
Ryan Shin <1982sdr@hanmail.net>
2016-01-28 14:02:32 +0900

    pythonstudy - bowling2 score계산처리 완료 + roll에서 pins값 유효한지 체크.

--- bowling2/bowling.py
+++ bowling2/bowling.py
@@ -12,18 +12,27 @@ class Bowling:
         self.turnStrike = -1
         self.turnSpare = -1
         self.scoreIdx = 0
+        
+        self.isPlaying = True
 
     def _isStrikeTil9(self, pins):  # 9프레임 까지 스트라이크
         return self.currentFrame <= 9 and self.turnInFrame == 1 and pins == 10
     def _isStrikeAt10(self, pins):
         return self.currentFrame == 10 and pins == 10
     def _isSpareTil9(self):
+        if self.turnInFrame == 2 and sum(self.turns[self.turn-1:self.turn+1]) > 10:
+            raise ValueError('스페어 처리 개수 이상의 pins를 입력하셨습니다.')
         return self.turnInFrame == 2 and sum(self.turns[self.turn-1:self.turn+1]) == 10
     def _isSpareAt10(self):
         if self.turnInFrame == 2:
+            if self.turns[self.turn-1] < 10 and sum(self.turns[self.turn-1:self.turn+1]) > 10:
+                raise ValueError('스페어 처리 개수 이상의 pins를 입력하셨습니다.')
             return self.turns[self.turn-1] < 10 and sum(self.turns[self.turn-1:self.turn+1]) == 10
         elif self.turnInFrame == 3:
-            return sum(self.turns[self.turn-2:self.turn])!= 10 and sum(self.turns[self.turn-1:self.turn+1]) == 10
+            if self.turns[self.turn-2] == 10 and self.turns[self.turn-1] < 10 and sum(self.turns[self.turn-1:self.turn+1]) > 10:
+                raise ValueError('스페어 처리 개수 이상의 pins를 입력하셨습니다.')
+            #return sum(self.turns[self.turn-2:self.turn])!= 10 and sum(self.turns[self.turn-1:self.turn+1]) == 10
+            return self.turns[self.turn-2] == 10 and self.turns[self.turn-1] < 10 and sum(self.turns[self.turn-1:self.turn+1]) == 10
         else:
             return False
         
@@ -33,10 +42,34 @@ class Bowling:
     def _getLastScore(self):
         lastScore = 0
         if self.scoreIdx > 0:
-             lastScore = self.scores[self.scoreIdx-1] 
+            lastScore = self.scores[self.scoreIdx-1] 
         return lastScore;
-
+    
+    def _caculPastStrike(self):
+        if self.turnStrike >=0 and self.turn == self.turnStrike + 2:
+            lastScore = self._getLastScore()
+            self.scores[self.scoreIdx] = lastScore + sum(self.turns[self.turnStrike:self.turn+1])
+            if self.scoreIdx < 9:
+                self.scoreIdx += 1
+            # 콤보 이상이면 strikeIdx 유지. 그렇지 않으면 초기화.
+            if self.turns[self.turnStrike+1] == 10:
+                self.turnStrike += 1
+            else:
+                self.turnStrike = -1
+    
+    def _caculPastSpare(self):
+        if self.turnInFrame == 1 and self.turnSpare >=0 and self.turn == self.turnSpare + 1:                
+            lastScore = self._getLastScore()
+            self.scores[self.scoreIdx] = lastScore + 10 + self.turns[self.turn]
+            self.scoreIdx += 1
+            self.turnSpare = -1
+                
     def roll(self, pins):
+        if pins != 'F' and (pins < 0 or pins > 10):
+            raise ValueError('pins 값은 F와 0과 10사이에 값만 허용됩니다.')
+        if self.isPlaying == False:
+            return
+        
         if pins == 'F':
             self.turns[self.turn] = 0
         else:
@@ -50,27 +83,23 @@ class Bowling:
                 # 스트라이크인 turn 위치를 기억해 놓음.
                 if self.turnStrike == -1:
                     self.turnStrike = self.turn
+            elif self._isSpareTil9():
+                # 심볼로직
+                self.symbols[self._getSymbolIndex()] = '/'
+                # 스페어인 turn 위치를 기억해 놓음.
+                if self.turnSpare == -1:
+                    self.turnSpare = self.turn
+            elif pins == 0:
+                # 심볼로직
+                self.symbols[self._getSymbolIndex()] = '-'
             else:
-                # 심볼로직   
-                if pins == 0:
-                    self.symbols[self._getSymbolIndex()] = '-'
-                elif self._isSpareTil9():
-                    self.symbols[self._getSymbolIndex()] = '/'
-                else:
-                    self.symbols[self._getSymbolIndex()] = pins                
+                # 심볼로직
+                self.symbols[self._getSymbolIndex()] = pins                
                 
             # 스트라이크 과거 점수처리    
-            if self.turnStrike >=0 and self.turn == self.turnStrike + 2:
-                lastScore = self._getLastScore()
-                self.scores[self.scoreIdx] = lastScore + sum(self.turns[self.turnStrike:self.turn+1])
-                self.scoreIdx += 1
-                # 콤보 이상이면 strikeIdx 유지. 그렇지 않으면 초기화.
-                if self.turns[self.turnStrike+1] == 10:
-                    self.turnStrike += 1
-                else:
-                    self.turnStrike = -1
+            self._caculPastStrike()
             # 스페어  과거 점수처리
-            
+            self._caculPastSpare()
             # 일반점수처리
             if self.turnInFrame == 2 and not self._isSpareTil9() and not self._isSpareTil9():
                 lastScore = self._getLastScore()
@@ -87,18 +116,37 @@ class Bowling:
                     self.currentFrame += 1
             
         elif self.currentFrame == 10:
-            # 심볼로직
+            # 전처리
+            # 심볼로직    
             if self._isSpareAt10(): # 스페어처리가 스트라이크 처리 계산보다 우선순위 높음.
                 self.symbols[self._getSymbolIndex()] = '/'
             elif self._isStrikeAt10(pins):
                 self.symbols[self._getSymbolIndex()] = 'X'
+            elif pins == 0:
+                self.symbols[self._getSymbolIndex()] = '-'
             else:
-                self.symbols[self._getSymbolIndex()] = pins
+                self.symbols[self._getSymbolIndex()] = pins           
             
-            self.turnInFrame += 1
-            
-            # 게임 끝났는지 조건 확인 필요.
             # 점수계산로직
+            # 스트라이크 과거 점수처리
+            self._caculPastStrike()
+            # 스페어  과거 점수처리
+            self._caculPastSpare()
+            
+            # 게임 끝났는지 조건 확인.
+            if self.turnInFrame == 2 and sum(self.turns[self.turn-1:self.turn+1]) < 10:
+                self.isPlaying = False
+            if self.turnInFrame == 3:
+                self.isPlaying = False
+                
+            # 게임이 종료하면 마지막 점수처리.
+            if self.isPlaying == False:
+                lastScore = self._getLastScore()
+                self.scores[self.scoreIdx] = lastScore + sum(self.turns[self.turn-self.turnInFrame+1:self.turn+1])
+                self.scoreIdx += 1
+            
+            # 후처리
+            self.turnInFrame += 1
             
         self.turn += 1
 
--- bowling2/testcase.py
+++ bowling2/testcase.py
@@ -1,3 +1,4 @@
+﻿# -*- coding: utf-8 -*-
 '''
 Created on 2016. 1. 24.
 
@@ -7,41 +8,20 @@ import unittest
 from bowling import Bowling
 
 class Test(unittest.TestCase):
-
     def setUp(self):
-        # given ?
+        # given
         self.b = Bowling()
 
     def tearDown(self):
         pass
 
-    # added
-    def testTurkey(self):
-        # when
-        self.b.roll(10)
-        self.b.roll(10)
-        self.b.roll(10)
-        # then
-        self.assertEqual(['X','','X','','X','','','','','','','','','','','','','','','',''], self.b.getSymbols())
-        self.assertEqual([30,'','','','','','','','',''], self.b.getScores())
+    def _repeatRoll(self, n, pins):
+        for i in range(n):
+            self.b.roll(pins)
         
-    def test9Combo(self):
-        # when
-        self.b.roll(10)
-        self.b.roll(10)
-        self.b.roll(10)
-        self.b.roll(10)
-        self.b.roll(10)
-        self.b.roll(10)
-        self.b.roll(10)
-        self.b.roll(10)
-        self.b.roll(10)
-        # then
-        self.assertEqual(['X','','X','','X','','X','','X','','X','','X','','X','','X','','','',''], self.b.getSymbols())
-        self.assertEqual([30,60,90,120,150,180,210,'','',''], self.b.getScores())
 
 
-    # original
+    # 문제에서 기본으로 주어진 테스트
     def testOneStrike(self):
         # when
         self.b.roll(10)
@@ -67,7 +47,7 @@ class Test(unittest.TestCase):
         self.b.roll(3)
         #then
         self.assertEqual(['X','',7,2,7,'/','','','','','','','','','','','','','','',''], self.b.getSymbols())
-    #    self.assertEqual([19,28,'','','','','','','',''], self.b.getScores())
+        self.assertEqual([19,28,'','','','','','','',''], self.b.getScores())
         
     def testLastSpare(self):
         # when
@@ -80,7 +60,7 @@ class Test(unittest.TestCase):
         self.b.roll(0)
         #then
         self.assertEqual(['X','',7,2,7,'/',9,'-','','','','','','','','','','','','',''], self.b.getSymbols())
-    #    self.assertEqual([19,28,47,'','','','','','',''], self.b.getScores())
+        self.assertEqual([19,28,47,56,'','','','','',''], self.b.getScores())
 
     def testFoul(self):
         # when
@@ -102,7 +82,8 @@ class Test(unittest.TestCase):
         self.b.roll('F')
         #then
         self.assertEqual(['X','',7,2,7,'/',9,'-',8,'/','X','',7,1,9,'-',9,'F','','',''], self.b.getSymbols())
-    
+        self.assertEqual([19,28,47,56,76,94,102,111,120,''], self.b.getScores())
+        
     def testFinish(self):
         # when
         self.b.roll(10)
@@ -127,7 +108,120 @@ class Test(unittest.TestCase):
         #then
         print self.b.getSymbols()
         self.assertEqual(['X','',7,2,7,'/',9,'-',8,'/','X','',7,1,9,'-',9,'F',9,'/',9], self.b.getSymbols())
+        self.assertEqual([19,28,47,56,76,94,102,111,120,139], self.b.getScores())
 
+    # 개인적으로 추가해본 테스트케이스들
+    def testTurkey(self):
+        # when
+        self._repeatRoll(3, 10)
+        # then
+        self.assertEqual(['X','','X','','X','','','','','','','','','','','','','','','',''], self.b.getSymbols())
+        self.assertEqual([30,'','','','','','','','',''], self.b.getScores())
+    def test9Combo(self):
+        # when
+        self._repeatRoll(9, 10)
+        # then
+        self.assertEqual(['X','','X','','X','','X','','X','','X','','X','','X','','X','','','',''], self.b.getSymbols())
+        self.assertEqual([30,60,90,120,150,180,210,'','',''], self.b.getScores())
+    def test10Combo(self):
+        # when
+        self._repeatRoll(10, 10)
+        # then
+        self.assertEqual(['X','','X','','X','','X','','X','','X','','X','','X','','X','','X','',''], self.b.getSymbols())
+        self.assertEqual([30,60,90,120,150,180,210,240,'',''], self.b.getScores())
+    def test11Combo(self):
+        # when
+        self._repeatRoll(11, 10)
+        # then
+        self.assertEqual(['X','','X','','X','','X','','X','','X','','X','','X','','X','','X','X',''], self.b.getSymbols())
+        self.assertEqual([30,60,90,120,150,180,210,240,270,''], self.b.getScores())
+    def testPerfectGame(self):
+        # when
+        self._repeatRoll(12, 10)
+        # then
+        self.assertEqual(['X','','X','','X','','X','','X','','X','','X','','X','','X','','X','X','X'], self.b.getSymbols())
+        self.assertEqual([30,60,90,120,150,180,210,240,270,300], self.b.getScores())
+    def testMissOne(self):
+        # when
+        self._repeatRoll(11, 10)
+        self.b.roll(9)
+
+        # then
+        self.assertEqual(['X','','X','','X','','X','','X','','X','','X','','X','','X','','X','X',9], self.b.getSymbols())
+        self.assertEqual([30,60,90,120,150,180,210,240,270,299], self.b.getScores())        
+    def testLastNoBonus(self):
+        # when
+        self._repeatRoll(9, 10)
+        self.b.roll(9)
+        self.b.roll(0)
+
+        # then
+        self.assertEqual(['X','','X','','X','','X','','X','','X','','X','','X','','X','',9,'-',''], self.b.getSymbols())
+        self.assertEqual([30,60,90,120,150,180,210,239,258,267], self.b.getScores())        
+    def testLastStrikeSpare(self):
+        # when
+        self._repeatRoll(10, 10)
+        self.b.roll(5)
+        self.b.roll(5)
+
+        # then
+        self.assertEqual(['X','','X','','X','','X','','X','','X','','X','','X','','X','','X',5,'/'], self.b.getSymbols())
+        self.assertEqual([30,60,90,120,150,180,210,240,265,285], self.b.getScores())        
+    def testLastSpareStrike(self):
+        # when
+        self._repeatRoll(9, 10)
+        self.b.roll(5)
+        self.b.roll(5)
+        self.b.roll(10)
+
+        # then
+        self.assertEqual(['X','','X','','X','','X','','X','','X','','X','','X','','X','',5,'/','X'], self.b.getSymbols())
+        self.assertEqual([30,60,90,120,150,180,210,235,255,275], self.b.getScores())
+    def testNineSpareTenStrikes(self):
+        # when
+        self._repeatRoll(8, 10)
+        self.b.roll(5)
+        self.b.roll(5)
+        self._repeatRoll(9, 10) # 3번만 해도 되는데 게임 끝났는지 로직이 잘되나 확인.
+        # then
+        self.assertEqual(['X','','X','','X','','X','','X','','X','','X','','X','',5,'/','X','X','X'], self.b.getSymbols())
+        self.assertEqual([30,60,90,120,150,180,205,225,245,275], self.b.getScores())
+
+    # 잘못된 수를 넣었을 때 Exception이 발생하는 지 여부.
+    # 참조 : http://stackoverflow.com/questions/156503/how-do-you-assert-that-a-certain-exception-is-thrown-in-junit-4-tests
+    # 질문 부분만 소스에서 참조함.
+    def testValueError(self):
+        thrown = False
+        try:
+            self.b.roll(-1)
+            self.b.roll(11)
+        except Exception:
+            thrown = True
+        self.assertTrue(thrown)
+    def testValueError2(self):
+        thrown = False
+        try:
+            self.b.roll('G')
+        except Exception:
+            thrown = True
+        self.assertTrue(thrown)
+    def testSpareError(self):
+        thrown = False
+        try:
+            self.b.roll(5)
+            self.b.roll(6)
+        except Exception:
+            thrown = True
+        self.assertTrue(thrown)
+    def testLastSpareError(self):
+        thrown = False
+        self._repeatRoll(10, 10)
+        try:
+            self.b.roll(5)
+            self.b.roll(6)
+        except Exception:
+            thrown = True
+        self.assertTrue(thrown)
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
